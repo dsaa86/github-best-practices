e65e9822d83b09ffc4245adbbff1266ee6bd7ade
Matthew Dunn <mattjohndunn@gmail.com>
2024-02-27 21:00:07 +0400

    Second Feature updated to generate random string. Tests for feature passing. Some tests from first feature failing.

--- third-feature/RandomGenerator.py
+++ third-feature/RandomGenerator.py
@@ -1,5 +1,9 @@
-from random import randint
+from random import choice, randint
 
 
 def generateRandomNumber(lower_bound: int = 0, upper_bound: int = 100):
     return randint(lower_bound, upper_bound)
+
+
+def generateRandomString(length: int = 10):
+    return ''.join(choice('abcdefghijklmnopqrstuvwxyz') for _ in range(length))
--- third-feature/tests/test_randomgenerator.py
+++ third-feature/tests/test_randomgenerator.py
@@ -1,5 +1,5 @@
 import unittest
-from ..RandomGenerator import generateRandomNumber
+from ..RandomGenerator import generateRandomNumber, generateRandomString
 
 class TestGenerateRandomNumber(unittest.TestCase):
     def test_generate_random_number(self):
@@ -11,6 +11,18 @@ class TestGenerateRandomNumber(unittest.TestCase):
         for _ in range(100):
             result = generateRandomNumber()
             self.assertTrue(0 <= result <= 100)
+
+    def test_generate_random_string_default_length(self):
+        result = generateRandomString()
+        self.assertEqual(len(result), 10)
+
+    def test_generate_random_string_custom_length(self):
+        result = generateRandomString(20)
+        self.assertEqual(len(result), 20)
+
+    def test_generate_random_string_content(self):
+        result = generateRandomString(1000)
+        self.assertTrue(all(c in 'abcdefghijklmnopqrstuvwxyz' for c in result))
 
 if __name__ == '__main__':
     unittest.main()
